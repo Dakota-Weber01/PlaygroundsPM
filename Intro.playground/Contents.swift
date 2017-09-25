b8142ec3ac137185a04739866124fae32ac169d3
Dakota Weber <dakota.weber01@gmail.com>
2017-09-25 13:29:38 -0600

    Attempted to add a scanner to getresponse from the user. Keyword Attempted.

--- Intro.playground/Contents.swift
+++ Intro.playground/Contents.swift
@@ -5,3 +5,19 @@ import UIKit
 var myName :String
 var otherName = "Words"
 var thisName : String = "meow"
+{
+var interogation : String
+print("So... um. Do you like cats? Yes or no?")
+let response = interogationResponse:
+let interogationResponse = Scanner.init(string: interogation)
+do CGPDFScannerScan(scanner: interogationResponse)
+if interogationResponse = ("Yes")
+Print("Hey, me too!")
+else if interogationResponse = ("No")
+Print("Lame, cats are cool.")
+
+}
+
+
+
+
