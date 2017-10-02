4f8319559c7c737e514ae47be0b7ab48e2299e90
Dakota Weber <dakota.weber01@gmail.com>
2017-10-02 12:24:52 -0600

    Added a sample class to the playground.

--- WhileIf.playground/Contents.swift
+++ WhileIf.playground/Contents.swift
@@ -44,3 +44,17 @@ while (test < counting())
     test += 1
 }
 print("yay!")
+// DIFFERENT NOTES STARTING HERE ------------------
+    var notesToday = "Using Swift to make classes and methods"
+
+public class SimpleClass
+{
+    private var name :String
+    public init()
+    {
+        name = "My name is ..."
+    }
+}
+
+
+
